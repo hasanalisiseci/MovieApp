6121c386357d52840a5d5501dbd8d9c1ce814465
Hasan Ali Şişeci <hasan.siseci@appcent.mobi>
2022-10-17 16:13:47 +0300

    initial commit

---
+++ MovieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Hasan Ali Şişeci on 17.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
